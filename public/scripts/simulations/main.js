4803c1f3c70fb8673eaeeaabfa03708a224d42d3
Myeloon <jjmorton1636@gmail.com>
2019-02-12 00:38:29 +0000

    Oscillations now only computes forces

--- public/scripts/simulations/main.js
+++ public/scripts/simulations/main.js
@@ -3,6 +3,40 @@
 	// A bunch of variables and functons that I'll want in all the simulations
 
 	window.Sim = {};
+	
+
+	// Get colours defined on root element in css
+	style = window.getComputedStyle(document.documentElement);
+
+	Sim.colours = {
+		background: style.getPropertyValue("--background-color"),
+		foreground: style.getPropertyValue("--text-color"),
+		accent: style.getPropertyValue("--accent-color")
+	};
+	
+
+	Sim.getLength = percent => Sim.canvas.height * percent / 100;
+
+
+	Sim.addButton = function(label, func) {
+		const container = document.getElementById("controls");
+		const btn = document.createElement("button");
+		btn.textContent = label;
+		btn.addEventListener("click", func);
+		container.appendChild(btn);
+	};
+
+	Sim.addSlider = function(label, obj, prop, min, max, step) {
+		const container = document.getElementById("controls");
+		const slider = document.createElement("input");
+		slider.setAttribute("type", "range");
+		slider.min = min;
+		slider.max = max;
+		slider.step = step;
+		slider.addEventListener("input", () => obj[prop] = parseFloat(slider.value));
+		slider.textContent = label;
+		container.appendChild(slider);
+	};
 
 
 	// This function should be set by the simulation
@@ -18,18 +52,7 @@
 		window.requestAnimationFrame(beginRender);
 	};
 
-	
-	// Get colours defined on root element in css
-	style = window.getComputedStyle(document.documentElement);
 
-	Sim.colours = {
-		background: style.getPropertyValue("--background-color"),
-		foreground: style.getPropertyValue("--text-color"),
-		accent: style.getPropertyValue("--accent-color")
-	}
-	
-
-	// This function can be called to get the canvas and its context
 	Sim.start = function() {
 
 		// Get the canvas element and context
@@ -38,11 +61,23 @@
 		
 		// Automatically resize the canvas with the window
 		const resizeCanvas = function() {
-			Sim.canvas.width = document.body.clientWidth;
-			Sim.canvas.height = window.innerHeight / window.innerWidth * Sim.canvas.width;
+			const size = Math.min(window.innerHeight * 0.8, document.body.clientWidth);
+			Sim.canvas.width = size;
+			Sim.canvas.height = size;
 		}
 		resizeCanvas();
 		window.addEventListener("resize", resizeCanvas);
+
+		// Track mouse
+		Sim.mouse = { pressed: -1, x: 0, y: 0 };
+		Sim.canvas.addEventListener("click", e => { if (Sim.onlick) Sim.onclick(e); });
+		Sim.canvas.addEventListener("mousedown", e => Sim.mouse.pressed = e.button);
+		Sim.canvas.addEventListener("mouseup", e => Sim.mouse.pressed = -1);
+		Sim.canvas.addEventListener("mousemove", e => {
+			Sim.mouse.x = 100 * e.clientX / Sim.canvas.height;
+			Sim.mouse.y = 100 * e.clientY / Sim.canvas.width;
+			if (Sim.onmousemove) Sim.onmousemove(e);
+		});
 
 		// Init colours
 		Sim.ctx.strokeStyle = Sim.colours.accent;
@@ -52,12 +87,6 @@
 		Sim.timeStart = performance.now();
 		if (Sim.init) Sim.init();
 		beginRender();
-
-		// Bind to buttons
-		document.getElementById("btn-reset").addEventListener("click", function() {
-			Sim.timeStart = performance.now();
-			if (Sim.init) Sim.init();
-		});
 	};
 
 }());
--- public/scripts/simulations/oscillations.js
+++ public/scripts/simulations/oscillations.js
@@ -2,7 +2,6 @@ window.addEventListener("load", function() {
 
 	'use strict';
 
-
 	const wavefunc = function(t, A, {m, k, b, phi}) {
 		// First convert all parameters from s to ms
 		k /= 1000000; // m s^-2
@@ -13,20 +12,45 @@ window.addEventListener("load", function() {
 
 
 	Sim.params = {
-		m: 1, // kg
+		m: 0.2, // kg
 		k: 200, // N m^-1
-		b: 1, // kg s^-1
-		phi: 0
+		b: 0, // kg s^-1
+	};
+
+	Sim.state = {
+		pos: 40, // Percent of height
+		vel: 0
+	};
+
+	window.getAcc = function() {
+		// First adjust units from s to ms
+		const k = Sim.params.k / 1000000;
+		const b = Sim.params.b / 1000;
+		return (-k * Sim.state.pos - b * Sim.state.vel) / Sim.params.m;
 	};
 
 	Sim.init = function() {
+		Sim.state.pos = 40;
+		Sim.state.vel = 0;
 	};
 
 	Sim.render = function() {
+		
+		Sim.state.vel += getAcc();
+		Sim.state.pos += Sim.state.vel;
+		
+		if (Sim.mouse.pressed === 0) Sim.state.pos = Math.max(Math.min(Sim.mouse.y, 90), 10) - 50;
+		const pos = Sim.getLength(Sim.state.pos + 50);
+
 		Sim.ctx.clearRect(0, 0, Sim.canvas.width, Sim.canvas.height);
-		const pos = wavefunc(Sim.time, Sim.canvas.height/2, Sim.params) + Sim.canvas.height / 2;
-		Sim.ctx.fillRect(Sim.canvas.width / 2 - 5, pos, 10, 10);
+		Sim.ctx.fillRect(Sim.getLength(49.5), 0, Sim.getLength(1), pos);
+		Sim.ctx.fillRect(Sim.getLength(47), pos, Sim.getLength(6), Sim.getLength(6));
 	};
+
+	Sim.addButton("Restart", () => Sim.init());
+	Sim.addSlider("k", Sim.params, "k", 10, 1000, 10);
+	Sim.addSlider("b", Sim.params, "b", 0, 10, 0.5);
+	Sim.addSlider("m", Sim.params, "m", 0.1, 3, 0.1);
 
 	Sim.start();
 
