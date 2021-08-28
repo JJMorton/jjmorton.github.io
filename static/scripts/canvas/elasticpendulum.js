51550e9203e28dd90b4b9d5cc063cd45fa246299
JJMorton <jjmorton1636@gmail.com>
2021-08-28 23:01:57 +0100

    Some changes to elasticpendulum

--- static/scripts/canvas/elasticpendulum.js
+++ static/scripts/canvas/elasticpendulum.js
@@ -8,7 +8,11 @@ window.addEventListener("load", function() {
 		g: 9.81,
 		m: 0.5,
 		k: 5.0,
-		l: 0.4
+		l: 0.8,
+		timestep: 0.01,
+		traillength: 500,
+		trailstep: 3,
+		showtrail: true
 	};
 
 	const state = {
@@ -16,8 +20,13 @@ window.addEventListener("load", function() {
 		theta: 0.8,
 		omega: 0,
 		x: params.m * params.g / params.k,
-		vx: 0
+		vx: 0,
+		trail: []
 	};
+
+	function getPivot() {
+		return [sim.pxToM(sim.canvas.width / 2), sim.pxToM(sim.canvas.height / 8)];
+	}
 
 	function eom_theta(t, theta, { omega, x, vx }) {
 		return omega;
@@ -63,23 +72,24 @@ window.addEventListener("load", function() {
 		sim.ctx.fill();
 	}
 
-	const TIME_STEP = 0.01;
 	sim.render = function() {
 
 		const time = sim.timer.getTime();
 
 		if (sim.mouse.pressed === 0) {
-			const x = sim.pxToM(sim.mouse.x - sim.canvas.width / 2);
-			const y = sim.pxToM(sim.mouse.y - sim.canvas.height / 2);
+			const [piv_x, piv_y] = getPivot();
+			const x = sim.pxToM(sim.mouse.x) - piv_x;
+			const y = sim.pxToM(sim.mouse.y) - piv_y;
 			const dist = Math.sqrt(x*x + y*y);
 			state.omega = 0;
 			state.theta = Math.atan2(x, y);
 			state.x = dist - params.l;
 			state.vx = 0;
+			state.trail = [];
 			state.lastupdate = time;
 		} else {
-			// Make sure that we recalculate the angle in time steps no larger than TIME_STEP
-			const divisions = Math.ceil((time - state.lastupdate) / TIME_STEP);
+			// Make sure that we recalculate the angle in time steps no larger than params.timestep
+			const divisions = Math.ceil((time - state.lastupdate) / params.timestep);
 			const deltat = (time - state.lastupdate) / divisions;
 			for (let t = state.lastupdate + deltat; t <= time; t += deltat) {
 				state.vx = rungekutta(eom_vx, state.vx, state.lastupdate, t, state);
@@ -91,24 +101,41 @@ window.addEventListener("load", function() {
 			}
 		}
 
-		const midx = sim.canvas.width / 2;
-		const midy = sim.canvas.height / 2;
+		const [midx, midy] = getPivot();
+		// const midx = sim.pxToM(sim.canvas.width / 2);
+		// const midy = sim.pxToM(sim.canvas.height / 10);
 
-		const x = sim.mToPx((params.l + state.x) * Math.sin(state.theta)) + midx;
-		const y = sim.mToPx((params.l + state.x) * Math.cos(state.theta)) + midy;
+		const x = (params.l + state.x) * Math.sin(state.theta) + midx;
+		const y = (params.l + state.x) * Math.cos(state.theta) + midy;
+		state.trail.push([x, y]);
+		while (state.trail.length > params.traillength) state.trail.shift();
 
 		sim.ctx.clearRect(0, 0, sim.canvas.width, sim.canvas.height);
 
-		drawline(midx, midy, x, y);
-		drawcircle(x, y, Math.pow(params.m * 1000, 1/3));
+		if (params.showtrail) {
+			for (let i = params.trailstep; i < state.trail.length - 1; i += params.trailstep) {
+				sim.ctx.globalAlpha = Math.sqrt(i / state.trail.length)
+				const start = state.trail[i - params.trailstep].map(q => sim.mToPx(q));
+				const end = state.trail[i].map(q => sim.mToPx(q));
+				drawline(start[0], start[1], end[0], end[1]);
+			}
+		}
+		sim.ctx.globalAlpha = 1;
+
+		const c_x = sim.mToPx(x);
+		const c_y = sim.mToPx(y);
+		sim.ctx.lineWidth = 2;
+		drawline(sim.mToPx(midx), sim.mToPx(midy), c_x, c_y);
+		sim.ctx.lineWidth = 1;
+		drawcircle(c_x, c_y, Math.pow(params.m * 1000, 1/3));
 
 	}
 
-	sim.addSlider("Scale", "m", sim.scale, 0.05, 20, 0.01, value => sim.scale = value);
-	sim.addSlider("Mass", "kg", params.m, 0.01, 5, 0.01, value => params.m = value);
-	sim.addSlider("Spring Constant", "N/m", params.k, 0.1, 20, 0.1, value => params.k = value);
-	sim.addSlider("Equilibrium Length", "m", params.l, 0.1, 5, 0.01, value => params.l = value);
-	sim.addSlider("Gravity", "m/s^2", params.g, 0, 20, 0.1, value => params.g = value);
+	sim.addSlider("m", "Mass", "kg", params.m, 0.01, 5, 0.01, value => params.m = value);
+	sim.addSlider("k", "Spring Constant", "N/m", params.k, 0.1, 20, 0.1, value => params.k = value);
+	sim.addSlider("l", "Equilibrium Length", "m", params.l, 0.1, 5, 0.01, value => params.l = value);
+	sim.addSlider("g", "Gravity", "m/s^2", params.g, 0, 20, 0.1, value => params.g = value);
+	sim.addCheckbox("trail", "Show Trail", params.showtrail, value => params.showtrail = value);
 
 	sim.start();
 
