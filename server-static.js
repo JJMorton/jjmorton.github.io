8f62c322625bbda2e26c4611d228e3357fbe05e7
JJMorton <jjmorton1636@gmail.com>
2022-06-18 13:24:36 +0100

    GitHub Actions fix

--- server-static.js
+++
@@ -1,9 +0,0 @@
-// Server for testing github pages 'docs' directory
-const express = require("express");
-const app = express();
-app.use(express.static(__dirname + "/docs"));
-app.listen(process.env.PORT || 8001);
-
-app.use((req, res) => {
-	res.status(404).sendFile(__dirname + "/docs/404.html");
-});
